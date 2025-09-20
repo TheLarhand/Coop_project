05b9b1b22be21e632507c62835be4f536bfdabce
Tiltushkin <ivlad6543@gmail.com>
2025-09-20 14:08:04 +0500

    fix: Убрал не нужный импорт "useRef"

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import MainLayout from '../../layouts/MainLayout.tsx'
-import React, {useRef, useState} from "react";
+import React, {useState} from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import type {AppDispatch} from "../../store/store.ts";
 import {login, selectIsAuthenticated} from "../../store/slices/authSlice.ts";
